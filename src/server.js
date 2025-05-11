f98705860697f9a6abda87faeac6de75bcd35d90
evgen_zoidze <1ovva.ungrace@gmail.com>
2025-05-11 08:38:53 +0200

    fix3

--- src/server.js
+++ src/server.js
@@ -46,11 +46,11 @@ export const setupServer = () => {
         });
     });
     
-    app.use('*', (req, res, next) => {
-        res.status(404).json({
-          message: 'Not found',
-        });
-      });
+    // app.use('*', (req, res, next) => {
+    //     res.status(404).json({
+    //       message: 'Not found',
+    //     });
+    //   });
     
     app.use((err, req, res, next) => {
         res.status(500).json({
