d137e49db544815e6ea83808dc7739f27047e50d
evgen_zoidze <1ovva.ungrace@gmail.com>
2025-05-18 11:17:55 +0200

    INIT

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import pino from "pino-http";
 import cors from "cors";
 import dotenv from "dotenv";
 import { getEnvVar } from "./utils/getEnvVar.js";
-import { getAllContacts, getContactbyId } from "./services/contacts.js";
+import contactsRouter from "./routers/contacts.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+// import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+
 
 dotenv.config();
 
@@ -20,46 +23,12 @@ export const setupServer = () => {
             },
         }),
     );
-    app.get('/contacts', async (req, res) => {
-        const contacts = await getAllContacts();
-
-        res.status(200).json({
-            status: 200,
-            message: "Successfully found contacts!",
-            data: contacts,
-          });
-      });
-
-    app.get("/contacts/:contactId", async (req, res, next) => {
-        const { contactId } = req.params;
-        const contact = await getContactbyId(contactId);
-
-        if (!contact) {
-            res.status(404).json({
-                message: "contact not found!",
-            });
-            return;
-        }
-
-        res.status(200).json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-        });
-    });
     
-    // app.use('*', (req, res, next) => {
-    //     res.status(404).json({
-    //       message: 'Not found',
-    //     });
-    //   });
+    app.use(contactsRouter);
+
+    // app.use("/{*splat}", notFoundHandler);
     
-    app.use((err, req, res, next) => {
-        res.status(500).json({
-          message: 'Something went wrong',
-          error: err.message,
-        });
-      });
+    app.use(errorHandler);
     
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
