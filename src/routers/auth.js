f6610377c6d5ff1e782bd0beda06cbec4658be48
evgen_zoidze <1ovva.ungrace@gmail.com>
2025-06-09 07:41:06 +0200

    fix

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -35,7 +35,7 @@ router.post("/logout", ctrlWrapper(logoutUserController));
 router.post("/refresh", ctrlWrapper(refreshUsersSessionController));
 
 router.post(
-  "/request-reset-email",
+  "/send-reset-email",
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController)
 );
