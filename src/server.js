a8f29431854b851d3594bc0f6e4b2576a04b9506
evgen_zoidze <1ovva.ungrace@gmail.com>
2025-06-15 12:33:03 +0200

    INIT

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import cookieParser from "cookie-parser";
 import { UPLOAD_DIR } from "./constants/index.js";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 
 dotenv.config();
@@ -29,10 +30,13 @@ export const setupServer = () => {
     
     app.use(router);
 
+    app.use("/api-docs", swaggerDocs());
+
     app.use("/{*splat}", notFoundHandler);
     
     app.use(errorHandler);
     app.use("/uploads", express.static(UPLOAD_DIR));
+    
     
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
